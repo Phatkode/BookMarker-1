ea6d3b40ac71287fb9dcb19a5d94aacaa2a1bb72
TSN Student <student@ulem.org>
2018-04-24 15:43:02 -0400

    Bookmarker 1

---
+++ BookmarkerApp.js
@@ -0,0 +1,31 @@
+
+
+
+// LISTENER
+document.querySelector('#myForm').addEventListener('submit', docEars);
+// listener test Function
+
+function docEars(e){
+		// console.log("docEars");
+
+
+
+var siteName = document.querySelector('#siteName').value;
+			   console.log(siteName);
+		// prevent from clearing console (need once)
+		e.preventDefault();
+
+
+var siteUrl = document.querySelector('#siteUrl').value;
+			   console.log(siteUrl);
+	
+
+
+
+// document.querySelector('#myForm').addEventListener('submit' saveBookmark);
+
+
+
+// function saveBookmark(){
+// 		console.log("saveBookmark");
+ }
